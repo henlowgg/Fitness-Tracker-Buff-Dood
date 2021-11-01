22fc178d38c6473537e8222b22da7af3f36e391e
Jack Ault <henlowgg@outlook.com>
2021-11-01 08:13:52 -0400

    routes gtg

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const path = require("path");
+
+// HTML GET Requests for all pages
+// =============================================================
+// Home page
+
+// may need to change this to ../public
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+// GET exercise page
+router.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// GET stats page
+router.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
