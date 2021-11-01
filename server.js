f702bc8e4ad32697f25eb99a5af1c305cf0635aa
Jack Ault <henlowgg@outlook.com>
2021-11-01 08:23:12 -0400

    app working and connected with mongo, minor issue with data not saving workout duration

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/populate', { useNewUrlParser: true });
+
+app.use(require('./routes/apiRoutes.js'));
+app.use(require('./routes/htmlRoutes.js'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
