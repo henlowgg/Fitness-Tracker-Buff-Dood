99c17773503b9ed74d36d792b703a1e9974f071d
Jack Ault <henlowgg@outlook.com>
2021-11-01 08:09:00 -0400

    put route for adding a new exercise

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,6 +12,7 @@ router.get("/api/workouts", (req, res) => {
         });
 });
 
+// GET route for getting workout range
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
         .then(dbWorkout => {
@@ -21,3 +22,27 @@ router.get("/api/workouts/range", (req, res) => {
             res.json(err);
         });
 });
+
+// POST route for saving a new workout
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+// PUT route for updating a new exercise
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
